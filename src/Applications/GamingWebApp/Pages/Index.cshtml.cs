d5f9a1771083f862b56fd57359c548b235fcb144
Loek <admin@admin>
2019-06-18 21:15:23 +0200

    remove old code

--- src/Applications/GamingWebApp/Pages/Index.cshtml.cs
+++ src/Applications/GamingWebApp/Pages/Index.cshtml.cs
@@ -1,33 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using GamingWebApp.Proxy;
+﻿using GamingWebApp.Proxy;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Polly.Timeout;
-using Refit;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace GamingWebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> logger;
-        private readonly IOptionsSnapshot<LeaderboardApiOptions> options;
         private readonly TelemetryClient telemetryClient;
         private readonly ILeaderboardClient proxy;
 
-        public IndexModel(IOptionsSnapshot<LeaderboardApiOptions> options,
-            TelemetryClient telemetryClient,
+        public IndexModel(TelemetryClient telemetryClient,
             ILeaderboardClient proxy, ILoggerFactory loggerFactory)
         {
             this.logger = loggerFactory.CreateLogger<IndexModel>();
-            this.options = options;
             this.telemetryClient = telemetryClient;
             this.proxy = proxy;
         }
@@ -39,7 +32,6 @@ namespace GamingWebApp.Pages
             Scores = new List<HighScore>();
             try
             {
-                //ILeaderboardClient proxy = RestService.For<ILeaderboardClient>(options.Value.BaseUrl);
                 using (var operation = telemetryClient.StartOperation<RequestTelemetry>("LeaderboardWebAPICall"))
                 {
                     try
